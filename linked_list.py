185e8d4371e3cd3e5e67ac32da46db13b78ed3d1
my-xh <2435128850@qq.com>
2019-08-28 18:47:57 +0800

    update linked_list.py

--- linked_list.py
+++ linked_list.py
@@ -10,38 +10,26 @@ class LinkedList:
         self.size = 0
 
     def insert_at_head(self, val):
-        cur = Node(val)
-        cur.next = self.head
-        self.head = cur
-        self.size += 1
+        self.insert_at(0, val)
 
     def insert_at_tail(self, val):
-        if self.is_empty():
-            self.add_at_head(val)
-        else:
-            p = self.head
-            while p.next is not None:
-                p = p.next
-            cur = Node(val)
-            p.next = cur
-            self.size += 1
+        self.insert_at(self.length(), val)
 
     def insert_at(self, idx, val):
         if idx < 0 or idx > self.length():
             raise IndexError('index out of range.')
-        if idx == 0:
-            self.insert_at_head(val)
-        elif idx == self.length():
-            self.insert_at_tail(val)
+        cur = Node(val)
+        if idx == 0 or self.is_empty():
+            cur.next = self.head
+            self.head = cur
         else:
             p = self.head
             for _ in range(idx - 1):
                 p = p.next
-            cur = Node(val)
             temp = p.next
             p.next = cur
             cur.next = temp
-            self.size += 1
+        self.size += 1
 
     def get_at(self, idx):
         if idx < 0 or idx >= self.length():
