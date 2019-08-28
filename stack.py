bd4be03d5fd7304f8597accdba02a2675a54e70d
my-xh <2435128850@qq.com>
2019-08-28 15:39:17 +0800

    update stack.py

--- stack.py
+++ stack.py
@@ -1,36 +1,37 @@
 class Stack:
-	def __init__(self, limit):
-		self.stack = []
-		self.limit = limit
+    def __init__(self, limit=10):
+        self.stack = []
+        self.limit = limit
 
-	def push(self, data):
-		if len(self.stack) >= self.limit:
-			print('StackOverflowError')
-		else:
-			self.stack.append(data)
+    def push(self, data):
+        if len(self.stack) >= self.limit:
+            print('StackOverflowError')
+        else:
+            self.stack.append(data)
 
-	def pop(self):
-		if not self.stack:
-			raise IndexError('pop from an empty stack')
-		else:
-			return self.stack.pop()
+    def pop(self):
+        if not self.stack:
+            # raise IndexError('pop from an empty stack')
+            return None
+        else:
+            return self.stack.pop()
 
-	def peek(self):
-		if self.stack:
-			return self.stack[-1]
-		else:
-			print('an empty stack')
+    def peek(self):
+        if self.stack:
+            return self.stack[-1]
+        else:
+            print('an empty stack')
 
-	def is_empty(self):
-		return not bool(self.stack)
+    def is_empty(self):
+        return not bool(self.stack)
 
-	def size(self):
-		return len(self.stack)
+    def size(self):
+        return len(self.stack)
 
-	def clear(self):
-		self.stack.clear()
+    def clear(self):
+        self.stack.clear()
 
-	def show(self):
-		for i in reversed(self.stack):
-			print('|{:^3d}|'.format(i))
-		print('=====')
+    def show(self):
+        for i in reversed(self.stack):
+            print('|{:^3d}|'.format(i))
+        print('=====')
